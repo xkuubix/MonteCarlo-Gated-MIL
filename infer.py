d47ed81b33b3e624be237004146fdc498e516a5a
Jakub Buler <buler.jakub@gmail.com>
2025-04-06 15:58:47 +0200

    change plot concept

--- infer.py
+++ infer.py
@@ -12,9 +12,9 @@ import seaborn as sns
 from torchvision import transforms as T
 
 
-def plot_attention_and_density(image, pos_att, neg_att, probs, item, save_path=None):
-    fig = plt.figure(figsize=(10, 8))
-    gs = fig.add_gridspec(2, 3, height_ratios=[1, 0.3])
+def plot_attention_and_density(image, pos_att, pos_std, neg_att, neg_std, probs, item, save_path=None):
+    fig = plt.figure(figsize=(10, 5))
+    gs = fig.add_gridspec(1, 5)
 
 
     neg_att_scaling_factor = probs[:,:,0].mean(dim=(0,1)).item()
@@ -29,120 +29,83 @@ def plot_attention_and_density(image, pos_att, neg_att, probs, item, save_path=N
     
     ax2 = fig.add_subplot(gs[0, 1])
     ax2.imshow(neg_att * neg_att_scaling_factor, cmap="Blues", vmin=0., vmax=1.)
-    ax2.set_title("Normal Attention")
+    ax2.set_title("Negative Attention")
     ax2.axis("off")
 
     ax3 = fig.add_subplot(gs[0, 2])
     ax3.imshow(pos_att * pos_att_scaling_factor, cmap="Reds", vmin=0., vmax=1.)
-    ax3.set_title("Cancer Attention")
+    ax3.set_title("Positive Attention")
     ax3.axis("off")
 
-    ax4 = fig.add_subplot(gs[1, :])
+    ax5 = fig.add_subplot(gs[0, 3])
+    ax5.imshow(neg_std**2, cmap="gray")
+    ax5.set_title("Negative Variance")
+    ax5.axis("off")
+    
+    ax6 = fig.add_subplot(gs[0, 4])
+    ax6.imshow(pos_std**2, cmap="gray")
+    ax6.set_title("Positive Variance")
+    ax6.axis("off")
 
     mc_preds = probs.squeeze().cpu().numpy()
-
     positive_probs = mc_preds[:, 1]
-    negative_probs = mc_preds[:, 0]
 
-    sorted_indices = np.argsort(positive_probs)
-    positive_probs_sorted = positive_probs[sorted_indices]
-    negative_probs_sorted = negative_probs[sorted_indices]
-
-    bars = np.arange(len(positive_probs_sorted))  # X-axis positions
-    bar_width = 1.0
-
-    ax4.bar(bars,
-            negative_probs_sorted,
-            bottom=positive_probs_sorted,
-            color="royalblue",
-            label="Negative (Normal)",
-            width=bar_width,
-            )
-    ax4.bar(bars,
-            positive_probs_sorted,
-            color="orangered",
-            label="Positive (Cancer)",
-            width=bar_width,
-            edgecolor='black',
-            linewidth=0.5
-            )
-
-    ax4.axhline(
-        0.5,
-        color="gold",
-        linestyle="-",
-        lw=1.5,
-        label="Threshold (p=0.5)"
-        )
-
-    x_min = bars[0] - 0.5 * bar_width
-    x_max = bars[-1] + 0.5 * bar_width
-    ax4.set_xlim(x_min, x_max)
-
-    x_middle = (x_min + x_max) / 2
-    ax4.axvline(
-        x=x_middle,
-        color="gold",
-        linestyle="--",
-        lw=1.5,
-        label="n/2"
-        )
-
-    ax4.set_xlim(bars[0] - 0.5 * bar_width, bars[-1] + 0.5 * bar_width)
-    ax4.set_xlabel(f"n={len(positive_probs_sorted)}")
-
-    ax4.set_ylim(0, 1)
-    ax4.set_ylabel("Probability", fontsize=10)
-    ax4.tick_params(
-        axis='x',
-        which='both',
-        bottom=False,
-        top=False,
-        labelbottom=False
-        )
     mean_pred = np.mean(positive_probs)
     median_pred = np.median(positive_probs)
     std_pred = np.std(positive_probs)
     iqr_pred = np.percentile(positive_probs, 75) - np.percentile(positive_probs, 25)
     min_pred, max_pred = np.min(positive_probs), np.max(positive_probs)
-
+    
+    entropy = -torch.sum(probs * torch.log(probs + 1e-10), dim=-1)
+    mean_entropy = entropy.mean()
+    def interpret_entropy(H):
+        if H < 0.2:
+            return "very low"
+        elif H < 0.4:
+            return "low"
+        elif H < 0.6:
+            return "moderate"
+        else:
+            return "high"
     stats_text = (
-        f"Probability of Cancer:    "
-        f"Mean: {mean_pred:.2f}    "
-        f"Std: {std_pred:.2f}    "
-        f"Median: {median_pred:.2f}    "
-        f"IQR: {iqr_pred:.2f}    "
-        f"Min: {min_pred:.2f}    Max: {max_pred:.2f}"
+        f"Probability of Cancer:     {mean_pred:.2f} "
+        f"({std_pred:.2f}) mean (std);     "
+        f"{median_pred:.2f} "
+        f"({iqr_pred:.2f}) median (iqr);     " 
+        f"{min_pred:.2f}-{max_pred:.2f} range;\n"
+        f"Mean Entropy: {mean_entropy:.2f} ({interpret_entropy(mean_entropy)} uncertainty)"
     )
 
     props = dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="white")
-    ax4.text(0.5, 0.95, stats_text, fontsize=9, 
-            verticalalignment='top', horizontalalignment='left', bbox=props)
+    
+    fig.text(0.5, -0.02, stats_text, fontsize=11, 
+            verticalalignment='center', horizontalalignment='center', bbox=props)
 
-    ax4.legend(
-        loc="upper center",
-        bbox_to_anchor=(0.5, 1.25),
-        ncol=4,
-        fontsize=10,
-        frameon=False
-        )
     title = (
-        f"Positive and Negative Attentions for {len(positive_probs_sorted)} Monte Carlo Dropout Samples \n"
-        f"Ground Truth: {item['target']['class'][0]}"
+        f"Positive and Negative Attentions for {len(positive_probs)} Monte Carlo Dropout Samples - "
+        f"Ground Truth: {item['target']['class'][0]}\n"
     )
     fig.suptitle(title)
     
     plt.tight_layout()
 
     if save_path:
-        plt.savefig(save_path, format="pdf", bbox_inches="tight", dpi=1_000)
-    plt.show()
+        plt.savefig(save_path+'.pdf', format="pdf", bbox_inches="tight", dpi=5_00)
+        plt.savefig(save_path+'.png', format="png", bbox_inches="tight", dpi=5_00)
+    # plt.show()
+    plt.close()
 
 
+# DEEEEEV
+# num_mc_passes = 100  # Replace with your actual number of MC passes
+# ys = torch.randn(num_mc_passes, 2)
+# probs = torch.nn.functional.softmax(ys, dim=-1).unsqueeze(0)
+# im = torch.zeros(3,7000,3000)
+# att = torch.zeros(7000,3000)
+# # plot_attention_and_density(image, pos_att, pos_std, neg_att, neg_std, probs, item, save_path=None)
+# plot_attention_and_density(im,att,att,att,att,probs,None)
 
-#TODO
-# MCDO / MCBN + MCDO
-
+# %%
 def deactivate_batchnorm(net):
     if isinstance(net, nn.BatchNorm2d):
         net.track_running_stats = False
